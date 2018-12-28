eb37cf3d94d7bf1612fffd59d59fb5bea70cc9bc
hzrenyabo <Ren19911013>
2018-12-28 16:48:07 +0800

    zookeeper.jar

--- zookeeper/src/main/java/org/yabo/zookeeper/App.java
+++ zookeeper/src/main/java/org/yabo/zookeeper/App.java
@@ -1,6 +1,10 @@
 package org.yabo.zookeeper;
 
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.commons.collections4.CollectionUtils;
 import org.yabo.common.Define;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -9,5 +13,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(Define.USER);
+        ZkClient client = new ZkClient("172.16.61.128:2181");
+        List<String> children = client.getChildren("/");
+        if (CollectionUtils.isNotEmpty(children)) {
+            children.forEach(System.out::println);
+        }
     }
 }
