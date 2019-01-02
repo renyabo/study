396254761bf0ab0349854b90b5aafdaee3a7f312
hzrenyabo <Ren19911013>
2019-01-02 17:39:49 +0800

    maven-over

--- zookeeper/src/main/java/org/yabo/zookeeper/App.java
+++ zookeeper/src/main/java/org/yabo/zookeeper/App.java
@@ -3,6 +3,7 @@ package org.yabo.zookeeper;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.commons.collections4.CollectionUtils;
 import org.yabo.common.Define;
+import org.yabo.common.Result;
 
 import java.util.List;
 
@@ -13,10 +14,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(Define.USER);
+        Result<List<String>> result = getResult();
+        System.out.println(result.getCode());
+
+        if (CollectionUtils.isNotEmpty(result.getData())) {
+            result.getData().forEach(System.out::println);
+        }
+    }
+
+    private static Result<List<String>> getResult() {
         ZkClient client = new ZkClient("172.16.61.128:2181");
         List<String> children = client.getChildren("/");
-        if (CollectionUtils.isNotEmpty(children)) {
-            children.forEach(System.out::println);
-        }
+        Result<List<String>> result = new Result<>();
+        result.setCode(200);
+        result.setMessage("OK");
+        result.setData(children);
+        return result;
     }
 }
