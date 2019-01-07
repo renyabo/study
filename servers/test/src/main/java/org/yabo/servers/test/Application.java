ab2d7cfd9af9eb90f660e88dd135d8e5ef24d5a6
hzrenyabo <Ren19911013>
2019-01-07 17:52:31 +0800

    整理pom

--- servers/test/src/main/java/org/yabo/servers/test/Application.java
+++ servers/test/src/main/java/org/yabo/servers/test/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @SpringBootConfiguration
 @ImportResource("classpath:dubbo.xml")
+@PropertySource("classpath:application.properties")
 public class Application {
 
     private static final Logger LOGGER = Logger.getLogger(Application.class);
