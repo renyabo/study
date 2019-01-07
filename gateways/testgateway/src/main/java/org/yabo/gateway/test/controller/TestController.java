b7d20070fd12117b7dce58f9a6c1ac8ce5be351a
hzrenyabo <Ren19911013>
2019-01-07 22:18:28 +0800

    test

--- gateways/testgateway/src/main/java/org/yabo/gateway/test/controller/TestController.java
+++ gateways/testgateway/src/main/java/org/yabo/gateway/test/controller/TestController.java
@@ -1,6 +1,5 @@
 package org.yabo.gateway.test.controller;
 
-import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
