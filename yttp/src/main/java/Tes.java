c192f9c712bac28787fe52fee7b7698afe845bc7
hzrenyabo <Ren19911013>
2019-03-12 19:49:31 +0800

    Revert "test"
    This reverts commit 2f828344

--- yttp/src/main/java/Tes.java
+++ yttp/src/main/java/Tes.java
@@ -3,6 +3,5 @@ import com.netflix.hystrix.HystrixCommand;
 public class Tes {
     public static void main(String[] args) {
         HystrixCommand hystrixCommand;
-        System.out.println("xxxx");
     }
 }
